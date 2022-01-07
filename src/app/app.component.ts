c1128f5938c00372b25b8dad7993f70944196408
Sean Perkins <sean@ionic.io>
2022-01-07 13:58:34 -0500

    chore(): reproduce issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+
+  items = [];
+
+  ngOnInit(): void {
+    this.items = Array(100).fill(undefined).map((_value, index) => `Item -- ${index} (${Date.now()})`);
+  }
+
+  onRefresh(event) {
+    this.items = Array(100).fill(undefined).map((_value, index) => `Item -- ${index} (${Date.now()})`);
+    setTimeout(() => {
+      event.detail.complete();
+    }, 2000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), ScrollingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
